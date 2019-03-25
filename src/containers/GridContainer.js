de245b0d57647bd577f721cf2b5490e8d0414c8c
Zak Stallings <zstallings@earnup.com>
2019-03-25 08:37:16 -0700

    add button docs. add dogs endpoint

--- src/containers/GridContainer.js
+++ src/containers/GridContainer.js
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
+
+/**
+ * https://material-ui.com/api/button/
+ * <Button
+ *  onClick={onClick}
+ * >
+ *   I'm a button!
+ * </Button>
+ */
 import Button from '@material-ui/core/Button';
 
 class GridContainer extends Component {
@@ -8,7 +17,7 @@ class GridContainer extends Component {
   }
 
   async componentDidMount() {
-    const results = await fetch('http://localhost:3001/cats')
+    const results = await fetch('http://localhost:3001/dogs')
     const data = await results.json();
     this.setState({ images: data })
   }
