8f3171748e10723a43cbaff247dbc2450bd5dc30
Zak Stallings <zakstal@gmail.com>
2019-03-24 19:44:55 -0700

    add fetch for images

--- src/containers/GridContainer.js
+++ src/containers/GridContainer.js
@@ -3,6 +3,16 @@ import _ from 'lodash';
 import Button from '@material-ui/core/Button';
 
 class GridContainer extends Component {
+  static state = {
+    images: []
+  }
+
+  async componentDidMount() {
+    const results = await fetch('http://localhost:3001/cats')
+    const data = await results.json();
+    this.setState({ images: data })
+  }
+
   render() {
 
     return (
